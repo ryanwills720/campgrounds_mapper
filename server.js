069122688b4920f754fe548074f4ec5bb5d01db2
Ryan Wills <68193699+ryanwills720@users.noreply.github.com>
2020-11-02 12:52:05 -0600

    added server.js and connection.js

---
+++ server.js
@@ -0,0 +1,26 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3001;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, ".\Map\map.html"));
+  });
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+});
