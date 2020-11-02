8ee3367c3827fbc8008470fa244aada2fafbf4e4
Katie R-A <kreidanderson@msn.com>
2020-11-02 13:48:29 -0600

    Site working locally

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
-// Dependencies
-// =============================================================
 var express = require("express");
-var path = require("path");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 8080;
+
+// Requiring our models for syncing
+var db = require("./models");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Static directory
+app.use(express.static("public"));
+
 // Routes
 // =============================================================
 
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, ".\Map\map.html"));
-  });
+// app.get("/", function(req, res) {  
+//   res.sendFile(path.join(__dirname, "Map/map.html"));
+// });
 
-// Starts the server to begin listening
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+// Syncing our sequelize models and then starting our Express app
 // =============================================================
-app.listen(PORT, function() {
+db.sequelize.sync().then(function() {
+
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
+
