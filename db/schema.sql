c43b0a4e12087ef4110b1977ba334a7827c18ea4
Zach Smelcer <zacaronithesmelcer@icloud.com>
2020-11-03 11:23:34 -0600

    Fixed the shema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,13 @@
 DROP DATABASE IF EXISTS ratings_db;
--- Creates the "blogger" database --
+
 CREATE DATABASE ratings_db;
 USE ratings_db;
 DROP TABLE campgrounds;
 
 CREATE TABLE campgrounds (
 id INT NOT NULL AUTO_INCREMENT,
-longitude DECIMAL(, 10) NOT NULL,
-latitude DECIMAL(15, 10) NOT NULL, 
+longitude VARCHAR(255) NOT NULL,
+latitude VARCHAR(255) NOT NULL, 
 name VARCHAR(255) NOT NULL,
 phone VARCHAR(25),
 dates_open VARCHAR(255),
@@ -16,4 +16,5 @@ city VARCHAR(30) NOT NULL,
 PRIMARY KEY (id)
 );
 
+commit;
 SELECT * FROM campgrounds;
