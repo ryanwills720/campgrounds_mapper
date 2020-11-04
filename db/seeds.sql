96bade7fe6431fc52e74eea6f96e2510221b9b94
Katie R-A <kreidanderson@msn.com>
2020-11-04 11:13:37 -0600

    switching to orm

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,10 @@
--- use ratings_db;
+use ratings_db;
 
--- CREATE TABLE ratings (
---     id int AUTO_INCREMENT
---     name 
---     rating  
--- )
+CREATE TABLE ratings (
+    id int AUTO_INCREMENT
+    name VARCHAR(25),
+    rating VARCHAR(25),
+)
 
 -- INSERT INTO ratings (text) VALUES ("5 stars");
 -- INSERT INTO ratings (text) VALUES ("4 stars");
--- models/rating.js
+++ models/rating.js
@@ -1,8 +1,24 @@
-module.exports = function(sequelize, DataTypes) {
-    var Rating = sequelize.define("Ratings", {
-        text: DataTypes.STRING,
-      
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var rating = {
+  all: function(cb) {
+    orm.all("ratings", function(res) {
+      cb(res);
     });
-    return Rating;
-  };
-  
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create("ratings", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("ratings", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = rating;
