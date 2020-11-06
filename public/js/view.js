61a85f7fe497595c8ea7d57edd7ee1371ea07413
Ryan Wills <68193699+ryanwills720@users.noreply.github.com>
2020-11-05 22:00:14 -0600

    hid API keys for NPS and MAPS in heroku .env

--- public/js/view.js
+++ public/js/view.js
@@ -2,50 +2,50 @@ for (var i = 0; i < nationalParks.length; i++) {
     $("#nationalParks").append(
         `<option value="${nationalParks[i].key}">${nationalParks[i].name}</option>`
     )
-}
+};
 
 for (var i = 0; i < stateCode.length; i++) {
     $("#statePick").append(
         `<option value="${stateCode[i]}">${stateCode[i]}</option>`
     )
-}
+};
 
 // need to update this to be on backend
-
-$("#nationalParks").on("change", function (event) {
+$("#nationalParks").on("change", event => {
     var parkCode = $("#nationalParks option:selected").val().trim();
-    var URL = process.env.NPS_API;
+    // begin server call for NPS API key
+    $.ajax("/apikey", {
+        type: "GET"
+    }).then(res => {
+        var URL = "https://developer.nps.gov/api/v1/parks?parkCode=" + parkCode + res.apikey; 
+        // calls NPS API for park information
+        $.ajax({
+            url: URL,
+            method: "GET"
+        }).then(response => {
 
-    $.ajax({
-        url: URL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response.data[0].addresses[0])
-        console.log(response.data[0].latitude)
-        console.log(response.data[0].longitude)
+            var parkName = response.data[0].fullName;
+            var description = response.data[0].description;
 
-        var parkName = response.data[0].fullName;
-        var description = response.data[0].description;
+            const namesOfActivitesArray = response.data[0].activities.map(activity => activity.name);
+            function allActivities() {
+                $('#activity-view').empty();
+                var addParkName = $("<h4>");
+                addParkName.text("Activities for: " + parkName);
+                $('#activity-view').append(addParkName);
 
-        const namesOfActivitesArray = response.data[0].activities.map(activity => activity.name);
-        function allActivities() {
-            $('#activity-view').empty();
-            var addParkName = $("<h4>");
-            addParkName.text("Activities for: " + parkName);
-            $('#activity-view').append(addParkName);
-
-            for (var i = 0; i < 5 && i < namesOfActivitesArray.length; i++) {
-                var addActivity = $("<li>");
-                addActivity.text(namesOfActivitesArray[i]);
-                $("#activity-view").append(addActivity);
+                for (var i = 0; i < 5 && i < namesOfActivitesArray.length; i++) {
+                    var addActivity = $("<li>");
+                    addActivity.text(namesOfActivitesArray[i]);
+                    $("#activity-view").append(addActivity);
+                }
+                var addDescription = $("<p>")
+                addDescription.text(description)
+                $('#activity-view').append(addDescription);
             }
-            var addDescription = $("<p>")
-            addDescription.text(description)
-            $('#activity-view').append(addDescription);
-        }
-        allActivities();
+            allActivities();
+        });
     });
-
 });
 
 $("#statePick").on("change", function (event) {
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -9,39 +9,56 @@ var app = express();
 var rating = require("../models/rating.js");
 // Routes
 // =============================================================
-module.exports = function(app) {
-  app.get("/", function(req, res) {
+module.exports = function (app) {
+  // route to get apikey for NPS API
+  app.get("/apikey", function (req, res) {
+    if (process.env.NPS_API) {
+      res.json({
+        apikey: process.env.NPS_API
+      })
+    } else {
+      console.log("not heroku no APIkey found")
+    }
+  });
+
+
+  app.get("/", function (req, res) {
     // If there is a state query param...
     if (req.query.state) {
       let stateSelected = req.query.state // ex. "CO"
 
-      // get campgrounds by state
-      orm.campgroundsByState(stateSelected, function(stateCampgrounds) {
-         res.render("index", {campgrounds: stateCampgrounds});
+      // get campgrounds by state, and send maps API key
+      orm.campgroundsByState(stateSelected, function (stateCampgrounds) {
+        res.render("index", {
+          mapsapikey: process.env.MAPS_API,
+          campgrounds: stateCampgrounds
+        });
       });
-    } else { // return no campgrounds for home page
-      res.render("index", {campgrounds: []});
+    } else { // return no campgrounds for home page, and send MAPS API key
+      res.render("index", {
+        mapsapikey: process.env.MAPS_API,
+        campgrounds: []
+      });
     }
-    
+
   });
 
   // If a user sends data to add a new character..
   // GET route for getting all of the todos
-  app.get("/api/campgrounds", function(req, res) {
+  app.get("/api/campgrounds", function (req, res) {
     // Write code here to retrieve all of the todos from the database and res.json them
-    db.Rating.findAll({}) .then(data=>{
+    db.Rating.findAll({}).then(data => {
       console.log(data);
       res.json(data);
     });
   });
 
-  // POST route for saving a new todo. We can create todo with the data in req.body
-  app.post("/api/ratings", function(req, res) {
+  app.post("/api/ratings", function (req, res) {
     rating.create([
       "camp_name", "rating"
     ], [
       req.body.camp_name, req.body.rating
-    ], function(result) {
+    ], function (result) {
       // Send back the ID of the new quote
       res.json({ id: result.insertId });
     });
@@ -49,25 +66,25 @@ module.exports = function(app) {
 
 };
 
-  // DELETE route for deleting ratings. We can get the id of the Rating to be deleted from
-  // req.params.id
-  app.delete("/api/ratings/:id", function(req, res) {
-    let id = req.params.id;
-    db.Rating.destroy({
-      where: {
-        id: id
-      }
-    }).then(results => res.json(results));
+// DELETE route for deleting ratings. We can get the id of the Rating to be deleted from
+// req.params.id
+app.delete("/api/ratings/:id", function (req, res) {
+  let id = req.params.id;
+  db.Rating.destroy({
+    where: {
+      id: id
+    }
+  }).then(results => res.json(results));
 
 
-  });
+});
 
-  // PUT route for updating ratings. We can get the updated Rating data from req.body
-  app.put("/api/ratings", function(req, res) {
-    db.Rating.update(req.body, {where: {id: req.body.id}}).then(results => res.json(results));
-
-
+// PUT route for updating ratings. We can get the updated Rating data from req.body
+app.put("/api/ratings", function (req, res) {
+  db.Rating.update(req.body, { where: { id: req.body.id } }).then(results => res.json(results));
 
 
-  });
+
+
+});
 
